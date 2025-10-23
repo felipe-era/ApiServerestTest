5cb18b1978b6a17d97aba403a61729fc9b3d9bc4
feveloper <felip@FEVOLEPER>
2025-10-23 10:09:01 -0300

    Second

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   timeout: 30_000,
   expect: { timeout: 10_000 },
   retries: 1,
-  workers: 1, // evita paralelismo agressivo contra a API pública
+  workers: 1, // evita paralelismo contra a API pública
   reporter: [
     ['list'],
     ['junit', { outputFile: 'reports/junit.xml' }],
@@ -13,7 +13,6 @@ export default defineConfig({
   ],
   use: {
     baseURL: process.env.BASE_URL || 'https://serverest.dev',
-    // Deixe se quiser padronizar, mas não é obrigatório para GET
     extraHTTPHeaders: {
       'Content-Type': 'application/json; charset=utf-8',
     },
